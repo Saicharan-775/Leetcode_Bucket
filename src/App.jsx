0fc06be7ef61dc08ce55ff846005e9c71d352c39
Saicharan <nagillasaicharan007@gmail.com>
2025-10-17 20:08:38 +0530

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,61 @@ import './App.css'
 import Dashboard from './pages/Dashboard'
 import Navbar from './components/Navbar'
 import "./index.css";
-function App() {
-  
-
-  return (
+import NormalBucket from './pages/NormalBucket'
+import StrictBucket from './pages/StrictBucket'
+import { createBrowserRouter,Navigate,RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
+import Analysis from './pages/Analysis';
+import Resourse from './pages/Resourse';
+const approuter=createBrowserRouter([
+  {
+    path:"/",
+    element:<Navigate to ="/dashboard"/>
+  },
+  {
+    path:"/login",
+    element:<Login/>,
+  },{
+    path:"/dashboard",
+    element:
     <>
-  
-          <Navbar/>
-          <Dashboard/>
+    <Navbar/>
+     <Dashboard/>,
     </>
-  )
+   
+  },
+  {
+    path:"/normal-bucket",
+    element:  <>
+    <Navbar/>
+     <NormalBucket/>,
+    </>
+  },
+   {
+    path:"/strict-bucket",
+    element:  <>
+    <Navbar/>
+     <StrictBucket/>,
+    </>
+  },
+  {
+    path:"/analysis",
+    element:  <>
+    <Navbar/>
+     <Analysis/>,
+    </>
+  },
+  {
+    path:"/resourses",
+    element:  <>
+    <Navbar/>
+     <Resourse/>,
+    </>
+  },
+])
+function App() {
+  return  <RouterProvider router={approuter} />;
+  
 }
 
 export default App
---
+++ src/pages/Resourse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Resourse = () => {
+  return (
+    <div>Resourse</div>
+  )
+}
+
+export default Resourse
